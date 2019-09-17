e11077a507c4592e19d56f8e757caf1afa5256ed
Budlee <mat.auburn@gmail.com>
2019-09-17 08:54:20 -0400

    Api description (#4)
    * Add in API operations
    
    * Add in API operations

--- src/main/java/com/jpgough/apiworkshop/controller/WorkshopController.java
+++ src/main/java/com/jpgough/apiworkshop/controller/WorkshopController.java
@@ -1,5 +1,6 @@
 package com.jpgough.apiworkshop.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkshopController {
 
     @GetMapping("/hello")
+    @ApiOperation("Hello World, used in the bootstrap of the lab")
     public String helloWorld() {
         return "Hello World";
     }
