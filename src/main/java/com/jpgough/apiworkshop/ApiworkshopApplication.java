87e11807de36bb03f9f455ee80c0b6e85ee95f49
MattyA <mat.auburn@gmail.com>
2020-02-24 17:10:52 -0500

    Add APIinfo to add version

--- src/main/java/com/jpgough/apiworkshop/ApiworkshopApplication.java
+++ src/main/java/com/jpgough/apiworkshop/ApiworkshopApplication.java
@@ -4,8 +4,10 @@ import com.google.common.base.Predicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,7 +22,13 @@ public class ApiworkshopApplication {
 
 	@Bean
 	public Docket api() {
+		ApiInfo apiInfo = new ApiInfoBuilder()
+				.title("Api Workshop Solution")
+				.version("1.1")
+				.description("This is the Api Solution for the Todo API")
+				.build();
 		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo)
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
